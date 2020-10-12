51308485702db129300bdb08347c3153691c22d7
shivaditya <shivadityaingale@gmail.com>
2020-10-12 11:38:58 +0530

    Added stdbool.h file to build project.
    The file wasnt found in library.
    https://stackoverflow.com/a/14405759

--- defs.h
+++ defs.h
@@ -13,7 +13,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
-#include <stdbool.h>
+#include "stdbool.h"
 
 #if defined _MSC_VER && _MSC_VER < 1800
 /* Work around lack of inttypes.h and strtoumax in older MSVC */
